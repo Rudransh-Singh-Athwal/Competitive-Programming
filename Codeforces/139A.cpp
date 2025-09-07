871d54d67f4b2873297a5bb97108e58d6c01ebcc
Rudransh Singh Athwal <rsathwal04@gmail.com>
2025-09-07 18:23:59 +0530

    OPTIMAL Codeforces 139A -> Petr and Book

--- Codeforces/139A.cpp
+++ Codeforces/139A.cpp
@@ -1,23 +1,28 @@
 // https://codeforces.com/contest/139/problem/A
 // Petr and Book
-// Codeforces 139A -> Petr and Book
 
 #include <bits/stdc++.h>
 using namespace std;
 
-void findDay(int pages, vector<int> pagesPerDay)
+void findDay(int pages, vector<int> dayWisePagesRead)
 {
+  int totalPagesInAWeek = 0;
+  for (auto it : dayWisePagesRead)
+  {
+    totalPagesInAWeek += it;
+  }
+
+  pages %= totalPagesInAWeek;
+  if (pages == 0)
+    pages = totalPagesInAWeek;
+
   for (int i = 1; i <= 7; i++)
   {
-    pages -= pagesPerDay[i];
+    pages -= dayWisePagesRead[i];
     if (pages <= 0)
     {
       cout << i << endl;
       break;
-    }
-    if (pages > 0 && i == 7)
-    {
-      i = 0;
     }
   }
 }
@@ -27,16 +32,16 @@ int main()
   int pages;
   cin >> pages;
 
-  vector<int> pagesPerDay(8, 0);
+  vector<int> dayWisePagesRead(8, 0);
 
   for (int i = 1; i <= 7; i++)
   {
     int p;
     cin >> p;
-    pagesPerDay[i] = p;
+    dayWisePagesRead[i] = p;
   }
 
-  findDay(pages, pagesPerDay);
+  findDay(pages, dayWisePagesRead);
 
   return 0;
 }
