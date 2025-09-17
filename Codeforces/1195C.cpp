32e4551251549a1006c20a5e40c25f63ad1c6a33
Rudransh Singh Athwal <rsathwal04@gmail.com>
2025-09-17 23:26:38 +0530

    Codeforces 1195C -> Basketball Exercise

--- Codeforces/1195C.cpp
+++ Codeforces/1195C.cpp
@@ -1,32 +1,31 @@
 // https://codeforces.com/problemset/problem/1195/C
 // Basketball Exercise
-// Codeforces 1195C -> Basketball Exercise
 
 #define ll long long
 #include <bits/stdc++.h>
 using namespace std;
 
-ll fn(int index, vector<int> &nums1, vector<int> &nums2, bool selectedRow, vector<vector<ll>> &dp)
-{
-  if (index >= nums1.size())
-    return 0;
+// ll fn(int index, vector<int> &nums1, vector<int> &nums2, bool selectedRow, vector<vector<ll>> &dp)
+// {
+//   if (index >= nums1.size())
+//     return 0;
 
-  if (dp[index][selectedRow] != -1)
-    return dp[index][selectedRow];
+//   if (dp[index][selectedRow] != -1)
+//     return dp[index][selectedRow];
 
-  ll notPick = fn(index + 1, nums1, nums2, selectedRow, dp);
-  ll pick = 0;
-  if (selectedRow)
-  {
-    pick = nums1[index] + fn(index + 1, nums1, nums2, !selectedRow, dp);
-  }
-  else
-  {
-    pick = nums2[index] + fn(index + 1, nums1, nums2, !selectedRow, dp);
-  }
+//   ll notPick = fn(index + 1, nums1, nums2, selectedRow, dp);
+//   ll pick = 0;
+//   if (selectedRow)
+//   {
+//     pick = nums1[index] + fn(index + 1, nums1, nums2, !selectedRow, dp);
+//   }
+//   else
+//   {
+//     pick = nums2[index] + fn(index + 1, nums1, nums2, !selectedRow, dp);
+//   }
 
-  return dp[index][selectedRow] = max(pick, notPick);
-}
+//   return dp[index][selectedRow] = max(pick, notPick);
+// }
 
 int main()
 {
@@ -34,7 +33,7 @@ int main()
   cin >> n;
   vector<int> nums1;
   vector<int> nums2;
-  vector<vector<ll>> dp(n, vector<ll>(2, -1));
+  vector<vector<ll>> dp(n + 1, vector<ll>(2, 0));
 
   for (int i = 0; i < n; i++)
   {
@@ -49,8 +48,27 @@ int main()
     nums2.push_back(x);
   }
 
-  ll startFromRow1 = fn(0, nums1, nums2, true, dp);
-  ll startFromRow2 = fn(0, nums1, nums2, false, dp);
+  for (int index = n - 1; index >= 0; index--)
+  {
+    for (int selectedRow = 0; selectedRow <= 1; selectedRow++)
+    {
+      ll notPick = dp[index + 1][selectedRow];
+      ll pick = 0;
+      if (selectedRow)
+      {
+        pick = nums1[index] + dp[index + 1][!selectedRow];
+      }
+      else
+      {
+        pick = nums2[index] + dp[index + 1][!selectedRow];
+      }
+
+      dp[index][selectedRow] = max(pick, notPick);
+    }
+  }
+
+  ll startFromRow1 = dp[0][true];
+  ll startFromRow2 = dp[0][false];
 
   ll ans = max(startFromRow1, startFromRow2);
   cout << ans << endl;
