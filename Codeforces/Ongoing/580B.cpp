c8f112558d61454aba53fb395f8d93917faa397e
Rudransh Singh Athwal <rsathwal04@gmail.com>
2025-10-25 13:05:47 +0530

    Memoization Codeforces 580B -> Kefa and Company

--- Codeforces/Ongoing/580B.cpp
+++ Codeforces/Ongoing/580B.cpp
@@ -6,55 +6,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-ll fn(int index, vector<pair<int, ll>> &nums, int maxi, int mini, const int maxD, unordered_map<int, unordered_map<uint64_t, ll>> &dp)
+ll fn(int index, int prev, const int d, vector<vector<int>> &nums, vector<vector<ll>> &dp)
 {
-  if (index < 0)
+  if (index >= nums.size() || (prev != -1 && nums[index][0] - nums[prev][0] >= d))
     return 0;
 
-  uint32_t high = static_cast<uint32_t>(static_cast<int32_t>(maxi));
-  uint32_t low = static_cast<uint32_t>(static_cast<int32_t>(mini));
-  uint64_t key = (static_cast<uint64_t>(high) << 32) | static_cast<uint64_t>(low);
+  if (prev != -1 && dp[index][prev] != -1)
+    return dp[index][prev];
 
-  if (dp.count(index) && dp[index].count(key))
-    return dp[index][key];
+  ll notPick = fn(index + 1, prev, d, nums, dp);
+  ll pick = nums[index][1] + fn(index + 1, (prev == -1 ? index : prev), d, nums, dp);
 
-  ll notPick = fn(index - 1, nums, maxi, mini, maxD, dp);
+  if (prev != -1)
+    dp[index][prev] = max(pick, notPick);
 
-  int money = nums[index].first;
-  ll friendshipFactor = nums[index].second;
-  ll pick = 0;
-
-  int newMax = (maxi == INT_MIN) ? money : max(maxi, money);
-  int newMin = (mini == INT_MAX) ? money : min(mini, money);
-
-  if ((ll)newMax - (ll)newMin <= (ll)maxD)
-  {
-    pick = friendshipFactor + fn(index - 1, nums, newMax, newMin, maxD, dp);
-  }
-
-  return dp[index][key] = max(pick, notPick);
+  return max(pick, notPick);
 }
 
 int main()
 {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  int n, maxD;
-  cin >> n >> maxD;
-  vector<pair<int, ll>> nums(n);
-  unordered_map<int, unordered_map<uint64_t, ll>> dp;
+  int n, d;
+  cin >> n >> d;
+  vector<vector<int>> nums(n, vector<int>(2)); // money, friendship factor
+  vector<vector<ll>> dp(n + 1, vector<ll>(n + 1, -1));
 
   for (int i = 0; i < n; i++)
   {
-    int x;
-    ll y;
-    cin >> x >> y;
-    nums[i].first = x;  // money
-    nums[i].second = y; // friendship factor
+    cin >> nums[i][0] >> nums[i][1];
   }
+  sort(nums.begin(), nums.end());
 
-  cout << fn(n - 1, nums, INT_MIN, INT_MAX, maxD, dp) << "\n";
+  ll ans = fn(0, -1, d, nums, dp);
+  cout << ans << endl;
 
   return 0;
 }
