5ca9ee271021d6911564796281b0f238f4ed351e
Rudransh Singh Athwal <rsathwal04@gmail.com>
2025-11-04 22:12:55 +0530

    NS Codeforces 112A -> Petya and Strings

---
+++ Codeforces/Not Submitted/112A.cpp
@@ -0,0 +1,32 @@
+// https://codeforces.com/problemset/problem/112/A
+// Petya and Strings
+// Codeforces 112A -> Petya and Strings
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s1, s2;
+    cin >> s1 >> s2;
+    for (auto &ch : s1)
+    {
+        ch = tolower(ch);
+    }
+    for (auto &ch : s2)
+    {
+        ch = tolower(ch);
+    }
+
+    if (s1 < s2)
+        cout << -1 << "\n";
+    else if (s2 < s1)
+        cout << 1 << "\n";
+    else
+        cout << 0 << "\n";
+
+    return 0;
+}
